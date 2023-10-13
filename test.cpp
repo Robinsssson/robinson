1f4229304d53fa7c76a9a4c1ab4bcb900eb18d82
Robinsssson <18656133516@qq.com>
2023-10-13 23:42:04 +0800

    Matrix
    Signed-off-by: Robinsssson <18656133516@qq.com>

---
+++ test.cpp
@@ -0,0 +1,29 @@
+//
+// Created by robinson on 2023/10/13.
+//
+#include <iostream>
+#include "matrix.h"
+
+using namespace robinson::matrix;
+
+void print_vec(std::vector<double> v) {
+    for (const auto &i: v) {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    Matrix m1(2, 3, {1, 2, 3, 4, 5, 6});
+    Matrix m2(3, 2, {1, 1, 1, 1, 1, 1});
+    m1.print();
+    m2.print();
+    Matrix m3;
+    Matrix m4;
+    m3 = m1 * m2;
+    m3.print();
+    std::cout << m1.at(1, 1) << std::endl;
+
+    return 0;
+}
+
